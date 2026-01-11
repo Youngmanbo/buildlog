1b6514bbb62ee7ddc2a9b717a761e1ed8cc6b483
Youngmanbo <yo900326@gmail.com>
2026-01-11 21:56:42 +0900

    settings 에 postgre db 설정

--- BuildLog/config/settings.py
+++ BuildLog/config/settings.py
@@ -74,12 +74,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "buildlog",
+        "USER": "postgre",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+        "CONN_MAX_AGE": 60,  # 연결 재사용
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
